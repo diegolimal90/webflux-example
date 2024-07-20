1efe4d6e26c0cb56a5b3a4b452b1e3ec6a802975
SDWeb <SDWeb>
2024-07-20 00:37:34 -0300

    example project

--- src/main/java/br/com/diego/webfluxexample/entrypoint/restclient/DebitosController.java
+++ src/main/java/br/com/diego/webfluxexample/entrypoint/restclient/DebitosController.java
@@ -6,14 +6,11 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 @RequestMapping("api/heroes")
 public class DebitosController {
-
-    private final ConcurrentLinkedQueue<String> eventQueue = new ConcurrentLinkedQueue<>();
 
 
     @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
